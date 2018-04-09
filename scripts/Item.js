9ee91578913fb747b4eaa6a27879863a08171734
Robert Brown <robertlb4@gmail.com>
2018-04-09 08:46:44 -0700

    add validatename and create to Item.js

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,7 +1,22 @@
 'use strict';
 
-const Item = (function() {
-  return {
+/* global cuid */
 
+const Item = (function() {
+  const validateName = function(name) {
+    if (!name) throw new TypeError('Name does not exist.');
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false,
+    };
+  };
+  
+  return {
+    validateName,
+    create,
   };
 }());
